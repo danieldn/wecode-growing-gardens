285a90248a5b12438251e80fd120f8145b232742
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-12 12:23:43 -0800

    initial login, signup, dashboard, added routes

--- webapp/app.py
+++ webapp/app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/slack')
 def slack():
     # with open('slack/report.json') as json_data:
